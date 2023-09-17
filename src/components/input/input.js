3bf55b117026f3dff99354b351b7a865da94eb79
Edward Kuan <edward880814@gmail.com>
2023-09-17 23:12:51 +0800

    feat: reset password logic

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,28 +1,27 @@
 import PropTypes from 'prop-types';
 import './Input.scss';
 
-const Input = ({ id, name, type, value, className, labelText, placeholder, handleChange }) => {
+const Input = ({ id, name, type, value, className, labelText, placeholder, handleChange, style }) => {
   return (
-    <>
-      <div className="form-row">
-        {labelText && (
-          <label htmlFor={name} className="form-label">
-            {labelText}
-          </label>
-        )}
+    <div className="form-row">
+      {labelText && (
+        <label htmlFor={name} className="form-label">
+          {labelText}
+        </label>
+      )}
 
-        <input
-          id={id}
-          name={name}
-          type={type}
-          value={value}
-          onChange={handleChange}
-          placeholder={placeholder}
-          className={`form-input ${className}`}
-          autoComplete="false"
-        />
-      </div>
-    </>
+      <input
+        id={id}
+        name={name}
+        type={type}
+        value={value}
+        onChange={handleChange}
+        placeholder={placeholder}
+        className={`form-input ${className}`}
+        style={style}
+        autoComplete="false"
+      />
+    </div>
   );
 };
 
@@ -34,7 +33,8 @@ Input.propTypes = {
   value: PropTypes.any,
   className: PropTypes.string,
   placeholder: PropTypes.string,
-  handleChange: PropTypes.func
+  handleChange: PropTypes.func,
+  style: PropTypes.object
 };
 
 export default Input;
