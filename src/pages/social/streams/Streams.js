d819f54a1582514b6cad2b9861fc00e4a019d11f
Edward Kuan <edward880814@gmail.com>
2023-12-22 19:26:41 +0800

    feat: streams pag component

--- src/pages/social/streams/Streams.js
+++ src/pages/social/streams/Streams.js
@@ -1,5 +1,22 @@
-const Streams = () => {
-  return <div>Streams Component</div>;
-};
+import { useRef } from 'react';
+import '@pages/social/streams/Streams.scss';
 
+const Streams = () => {
+  const bodyRef = useRef(null);
+  const bottomLineRef = useRef();
+  return (
+    <div className="streams" data-testid="streams">
+      <div className="streams-content">
+        <div className="streams-post" ref={bodyRef} style={{ backgroundColor: 'white' }}>
+          <div>Post Form</div>
+          <div>Posts Items</div>
+          <div ref={bottomLineRef} style={{ marginBottom: '50px', height: '50px' }}></div>
+        </div>
+        {/* <div className="streams-suggestions">
+          <Suggestions />
+        </div> */}
+      </div>
+    </div>
+  );
+};
 export default Streams;
