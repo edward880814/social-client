84838300eb74391be1facb46bb9c679aa1677e7b
Edward Kuan <edward880814@gmail.com>
2023-12-25 21:13:52 +0800

    feat: setup socketIO event

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { AppRouter } from '@root/routes';
 import { BrowserRouter } from 'react-router-dom';
+import { useEffect } from 'react';
 import '@root/App.scss';
+import { socketService } from '@services/socket/socket.service';
 
 const App = () => {
+  useEffect(() => {
+    socketService.setupSocketConnection();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
