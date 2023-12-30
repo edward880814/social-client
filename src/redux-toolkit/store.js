d9a0a90514b6531963a3054bdf833f62a9633491
Edward Kuan <edward880814@gmail.com>
2023-12-30 16:10:28 +0800

    feat: modal reducer

--- src/redux-toolkit/store.js
+++ src/redux-toolkit/store.js
@@ -2,10 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '@redux/reducers/user/user.reducer';
 import suggestionsReducer from '@redux/reducers/suggestions/suggestions.reducer';
 import notificationReducer from '@redux/reducers/notifications/notification.reducer';
+import modalReducer from '@redux/reducers/modal/modal.reducer';
+
 export const store = configureStore({
   reducer: {
     user: userReducer,
     suggestions: suggestionsReducer,
-    notification: notificationReducer
+    notification: notificationReducer,
+    modal: modalReducer
   }
 });
