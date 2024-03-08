5a1579d035a7bfa3dab484e20abfb05e1cb4bd4d
RD-dev-01\edward <edward880814@gmail.com>
2024-03-08 16:31:15 +0800

    feat: Posts and single post components

--- src/services/utils/utils.service.js
+++ src/services/utils/utils.service.js
@@ -77,4 +77,14 @@ export class Utils {
     }
     return `https://res.cloudinary.com/dbymmegbb/image/upload/v${version}/${id}`;
   }
+
+  static generateString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
 }
