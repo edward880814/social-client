3969652c23a59358b050ca55ef427f7d4c448a0f
RD-dev-01\edward <edward880814@gmail.com>
2024-03-11 15:44:02 +0800

    feat: reactions display component

--- src/components/posts/post-comment-section/PostCommentSection.js
+++ src/components/posts/post-comment-section/PostCommentSection.js
@@ -1,9 +1,11 @@
 import CommentArea from '@components/posts/comment-area/CommentArea';
 import PropTypes from 'prop-types';
+import ReactionsAndCommentsDisplay from '../reactions/reactions-and-comments-display/ReactionsAndCommentsDisplay';
 
 const PostCommentSection = ({ post }) => {
   return (
     <div data-testid="comment-section">
+      <ReactionsAndCommentsDisplay post={post} />
       <CommentArea post={post} />
     </div>
   );
