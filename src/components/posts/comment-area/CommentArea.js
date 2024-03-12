8200197de57ff5ac5f756569de1618bc58518849
RD-dev-01\edward <edward880814@gmail.com>
2024-03-12 16:53:31 +0800

    feat: add post reaction

--- src/components/posts/comment-area/CommentArea.js
+++ src/components/posts/comment-area/CommentArea.js
@@ -3,14 +3,19 @@ import { FaRegCommentAlt } from 'react-icons/fa';
 import '@components/posts/comment-area/CommentArea.scss';
 import Reactions from '../reactions/Reactions';
 import { useCallback, useEffect, useState } from 'react';
-import { find } from 'lodash';
+import { cloneDeep, filter, find } from 'lodash';
 import { Utils } from '@services/utils/utils.service';
 import { reactionsMap } from '@services/utils/static.data';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { postService } from '@services/api/post/post.service';
+import { socketService } from '@services/socket/socket.service';
+import { addReactions } from '@redux/reducers/post/user-post-reaction.reducer';
 
 const CommentArea = ({ post }) => {
-  const { reactions } = useSelector((state) => state.userPostReactions);
+  const { profile } = useSelector((state) => state.user);
+  let { reactions } = useSelector((state) => state.userPostReactions);
   const [userSelectedReaction, setUserSelectedReaction] = useState('');
+  const dispatch = useDispatch();
 
   const selectedUserReaction = useCallback(
     (postReactions) => {
@@ -21,7 +26,102 @@ const CommentArea = ({ post }) => {
     [post]
   );
 
-  const addReactionPost = async (reaction) => {};
+  const addReactionPost = async (reaction) => {
+    try {
+      const reactionResponse = await postService.getSinglePostReactionByUsername(post?._id, profile?.username);
+      post = updatePostReactions(
+        reaction,
+        Object.keys(reactionResponse.data.reactions).length,
+        reactionResponse.data.reactions?.type
+      );
+
+      const postReactions = addNewReaction(
+        reaction,
+        Object.keys(reactionResponse.data.reactions).length,
+        reactionResponse.data.reactions?.type
+      );
+      reactions = [...postReactions];
+      dispatch(addReactions(reactions));
+
+      sendSocketIOReactions(
+        post,
+        reaction,
+        Object.keys(reactionResponse.data.reactions).length,
+        reactionResponse.data.reactions?.type
+      );
+
+      const reactionsData = {
+        userTo: post?.userId,
+        postId: post?._id,
+        type: reaction,
+        postReactions: post.reactions,
+        profilePicture: profile?.profilePicture,
+        previousReaction: Object.keys(reactionResponse.data.reactions).length
+          ? reactionResponse.data.reactions?.type
+          : ''
+      };
+
+      if (!Object.keys(reactionResponse.data.reactions).length) {
+        await postService.addReaction(reactionsData);
+      } else {
+        reactionsData.previousReaction = reactionResponse.data.reactions?.type;
+        if (reaction === reactionsData.previousReaction) {
+          await postService.removeReaction(post?._id, reactionsData.previousReaction, post.reactions);
+        } else {
+          await postService.addReaction(reactionsData);
+        }
+      }
+    } catch (error) {
+      Utils.dispatchNotification(error?.response?.data?.message, 'error', dispatch);
+    }
+  };
+
+  const updatePostReactions = (newReaction, hasResponse, previousReaction) => {
+    post = cloneDeep(post);
+    if (!hasResponse) {
+      post.reactions[newReaction] += 1;
+    } else {
+      if (post.reactions[previousReaction] > 0) {
+        post.reactions[previousReaction] -= 1;
+      }
+      if (previousReaction !== newReaction) {
+        post.reactions[newReaction] += 1;
+      }
+    }
+    return post;
+  };
+
+  const addNewReaction = (newReaction, hasResponse, previousReaction) => {
+    const postReactions = filter(reactions, (reaction) => reaction?.postId !== post?._id);
+    const newPostReaction = {
+      avatarColor: profile?.avatarColor,
+      createdAt: `${new Date()}`,
+      postId: post?._id,
+      profilePicture: profile?.profilePicture,
+      username: profile?.username,
+      type: newReaction
+    };
+    if (hasResponse && previousReaction !== newReaction) {
+      postReactions.push(newPostReaction);
+    } else if (!hasResponse) {
+      postReactions.push(newPostReaction);
+    }
+    return postReactions;
+  };
+
+  const sendSocketIOReactions = (post, reaction, hasResponse, previousReaction) => {
+    const socketReactionData = {
+      userTo: post.userId,
+      postId: post._id,
+      username: profile?.username,
+      avatarColor: profile?.avatarColor,
+      type: reaction,
+      postReactions: post.reactions,
+      profilePicture: profile?.profilePicture,
+      previousReaction: hasResponse ? previousReaction : ''
+    };
+    socketService?.socket?.emit('reaction', socketReactionData);
+  };
 
   useEffect(
     () => {
@@ -34,7 +134,7 @@ const CommentArea = ({ post }) => {
   return (
     <div className="comment-area" data-testid="comment-area">
       <div className="like-icon reactions">
-        <div className="likes-block">
+        <div className="likes-block" onClick={() => addReactionPost('like')}>
           <div className={`likes-block-icons reaction-icon ${userSelectedReaction.toLowerCase()}`}>
             {userSelectedReaction && (
               <div className={`reaction-display ${userSelectedReaction.toLowerCase()}`} data-testid="selected-reaction">
