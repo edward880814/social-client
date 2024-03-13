31f44b8895d4a459a877c82da950f420b5e2f26b
RD-dev-01\edward <edward880814@gmail.com>
2024-03-13 10:41:43 +0800

    feat: display reactions

--- src/services/utils/utils.service.js
+++ src/services/utils/utils.service.js
@@ -2,6 +2,7 @@ import { addNotification, clearNotification } from '@redux/reducers/notification
 import { addUser, clearUser } from '@redux/reducers/user/user.reducer';
 import { avatarColors } from '@services/utils/static.data';
 import { floor, random, some } from 'lodash';
+import millify from 'millify';
 
 export class Utils {
   static avatarColor() {
@@ -99,5 +100,27 @@ export class Utils {
   static firstLetterUpperCase(word) {
     if (!word) return '';
     return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+  }
+
+  static formattedReactions(reactions) {
+    const postReactions = [];
+    for (const [key, value] of Object.entries(reactions)) {
+      if (value > 0) {
+        const reactionObject = {
+          type: key,
+          value
+        };
+        postReactions.push(reactionObject);
+      }
+    }
+    return postReactions;
+  }
+
+  static shortenLargeNumbers(data) {
+    if (data === undefined) {
+      return 0;
+    } else {
+      return millify(data);
+    }
   }
 }
